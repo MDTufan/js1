20fc17536606c746d4da073b2e389f6e540fb55f
Tufan ali <alitufan702@gmail.com>
2023-08-01 20:51:40 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// navigator.geolocation.getCurrentPosition(function(position){
+//     let num1=position.coords.latitude;
+//     let num2=position.coords.longitude;
+//     document.write(num1 +'<br>');
+// document.write(num2);
+// })
+
+// let myvideo=document.querySelector('video');
+// if(navigator.mediaDevices){
+//     navigator.mediaDevices.getUserMedia({
+//         video:true
+       
+//     } )
+//     console.log('yes');
+// }
+console.log('hello');
